181a185bc638b59ae5138cf686dd9592208ad21d
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:13:39 +0300

    Наследование от класса Car к классу HybridCar

--- inheritancePractice_7.1/Program.cs
+++ inheritancePractice_7.1/Program.cs
@@ -19,5 +19,41 @@
 
     }
 
+    class Car
+    {
+        public double Fuel;
+        public int Mileage;
+
+        public Car()
+        {
+            Fuel = 50;
+            Mileage = 0;
+        }
+        public void Move()
+        {
+            // Передвижение на километр
+            Mileage++;
+            Fuel -= 0.5;
+        }
+
+        public void FillTheCaer()
+        {
+            Fuel = 50;
+        }
+    }
+    enum FuelType
+    {
+        Gas = 0,
+        Electricity
+    }
+    class HybridCar : Car
+    {
+        public FuelType FuelType;
+
+        public void ChangeFuelType(FuelType type) 
+        { 
+            FuelType = type;
+        }
+    }
 
 }
