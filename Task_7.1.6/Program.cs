c114ae098c23857467fbf10ea01c732ac8837da5
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:51:17 +0300

    Выполнил задание 7.1.6

---
+++ Task_7.1.6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_7._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+}
