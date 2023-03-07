c0e3a879fccb5cc20885b7bc106972cebaa51a9e
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:28:04 +0300

    Выполнил запдание 7.1.4

---
+++ Task_7.1.4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_7._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Emplyee
+    {
+        public string Name;
+        public int Age;
+        public int Salary;
+    }
+    class ProjectManager : Emplyee
+    {
+        public string ProjectName;
+    }
+    class Developer : Emplyee
+    {
+        public string ProgrammingLanguage;
+    }
+}
