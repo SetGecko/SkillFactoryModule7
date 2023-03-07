b73c2b56f4d315aee90bee16f95865fb704e5e41
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 18:28:17 +0300

    Практика преобразования типов

---
+++ TypeConversion_7.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TypeConversion_7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HomoSapiens hs = new HomoSapiens();
+            Human human = hs;
+            Creature creature = (Creature)human;
+            Creature secondaryCreature = new Animal();
+
+        }
+    }
+    class Creature { }
+    class Animal : Creature { }
+    class Human : Creature { }
+    class HomoSapiens : Human { }
+}
