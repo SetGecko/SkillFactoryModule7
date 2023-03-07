366632a7a73e5a8514ab3b5e62902521476bf840
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 20:53:32 +0300

    Практика полиморфизма + выполнил задание 7.2.3

---
+++ Polymorphism_7.2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Polymorphism_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            HybridCar hybridCar = new HybridCar();
+
+            car.Move();
+            hybridCar.Move();
+        }
+    }
+    enum FuelType
+    {
+        Gas = 0,
+        Electricity
+    }
+    class Car
+    {
+        public int Mileage;
+        public Car()
+        {
+            Mileage = 0;
+        }
+        public virtual void Move()
+        {
+            Console.WriteLine("Вызван метод Move класса Car");
+            Mileage++;
+        }
+    }
+    class HybridCar: Car
+    {
+        public FuelType FuelType;
+        public double Gas;
+        public double Electricity;
+        
+        public HybridCar()
+        {
+            Electricity = 50;
+            Gas = 50;
+        }
+        public override void Move()
+        {
+            Console.WriteLine("Вызван метод Move класса HybridCar");
+            Mileage++;
+            switch(FuelType)
+            {
+                case FuelType.Gas:
+                    Gas -= 0.5;
+                    break;
+                case FuelType.Electricity:
+                    Electricity -= 0.5; 
+                    break;
+            }
+        }
+        public void ChangeFuelType(FuelType type)
+        {
+            FuelType = type;
+        }
+    }
+}
---
+++ Task_7.2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_7._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+    class DrivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DrivedClass");
+        }
+    }
+}
