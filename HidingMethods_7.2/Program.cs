3323aa9189078fda06f702eaa2e4e0d4aed2a789
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 21:59:26 +0300

    Практика скрытие методов

---
+++ HidingMethods_7.2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HidingMethods_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+
+            a.Display();    // метод класса A
+            b.Display();    // метод класса B
+            ((A)b).Display();   // метод класса B
+
+            c.Display();    // метод класса C
+            ((A)c).Display();   // метод класса B
+            ((B)c).Display();	// метод класса B
+        }
+    }
+    class A
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Вызван метод класса A");
+        }
+    }
+    class B : A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Вызван метод класса B");
+        }
+    }
+    class C : B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Вызван метод класса C");
+        }
+    }
+}
