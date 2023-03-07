7b7041134f68fe7ef52ba70495973be9ce1c5f9b
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:49:00 +0300

    Практика по ключевым словам this и base

---
+++ KeywordsThisAndBase_7.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace KeywordsThisAndBase_7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Obj
+    {
+        private string name;
+        private string description;
+
+        public Obj()
+        {
+            Console.WriteLine("Вызван конструктор без параметров");
+        }
+        public Obj(string name, string description)
+        {
+            //явное указание обращения к полям экзмепляра данного класса
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
