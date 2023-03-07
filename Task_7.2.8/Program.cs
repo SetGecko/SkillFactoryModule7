cfa50cb50f275c6035d7bcd1a6a31ca3779e7bce
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 22:09:29 +0300

    Решение задания 7.2.9

--- Task_7.2.8/Program.cs
+++ Task_7.2.8/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            D d = new D();
+            E e = new E();
+
+            d.Display();
+            ((A)e).Display();
+            ((B)d).Display();
+            ((A)d).Display();
         }
     }
     class A
