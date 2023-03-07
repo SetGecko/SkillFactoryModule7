48f37f152b3cab43fb0aa01282d03f5af0c29e8f
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:59:09 +0300

    Практика по ключевому слову base

--- KeywordsThisAndBase_7.1/Program.cs
+++ KeywordsThisAndBase_7.1/Program.cs
@@ -7,6 +7,25 @@
 
         }
     }
+    class BaseClass
+    {
+        public string Name;
+        protected string Description;
+        private int value;
+        public BaseClass(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    class InheritedClass : BaseClass
+    {
+        private int newValue;
+        public InheritedClass(int newValue) : base(100)
+        {
+            this.newValue = newValue;
+        }
+    }
     class Obj
     {
         private string name;
