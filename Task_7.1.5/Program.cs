a308c6339b3a4c8145b432525796b714dc9508d2
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 17:32:00 +0300

    Выполнил задлание 7.1.5

---
+++ Task_7.1.5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_7._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Food
+    {
+
+    }
+    class Fruit : Food
+    {
+
+    }
+    class Vegetable : Food
+    {
+
+    }
+    class Potato : Vegetable
+    {
+
+    }
+    class Carrot : Vegetable
+    {
+
+    }
+    class Apple : Fruit
+    {
+
+    }
+    class Banana : Fruit
+    {
+
+    }
+    class Pear : Fruit
+    {
+
+    }
+}
