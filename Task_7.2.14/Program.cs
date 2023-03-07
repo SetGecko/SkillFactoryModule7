0bc798f22f4bfd8eb494583e20b6e7e759a86f28
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 22:28:52 +0300

    Выполнил задание 7.2.14

---
+++ Task_7.2.14/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_7._2._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+}
