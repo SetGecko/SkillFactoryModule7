401b03f46049f36f68832ac842d5d3a9a3e1f3a8
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 22:18:20 +0300

    Выполнил задание 7.2.12

---
+++ Task_7.2.12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_7._2._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Obj
+    {
+        public int Value;
+
+        public static Obj operator +(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value + b.Value
+            };
+        }
+        public static Obj operator -(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value - b.Value
+            };
+        }
+    }
+}
