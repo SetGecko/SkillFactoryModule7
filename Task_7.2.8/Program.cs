6df31f92e4cd3ea22e37b4877a92c3ad26dd7524
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 22:04:58 +0300

    Выполнил задание 7.2.8

---
+++ Task_7.2.8/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task_7._2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class A
+    {
+        //Добавьте в класс A виртуальный метод Display (void тип, без параметров),
+        //который будет выводить в консоль "A".
+        public virtual void Display()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B : A
+    {
+        // В классе B скройте этот метод и сделайте так, чтобы в консоль выводилось "B".
+        public new void Display()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class C : A
+    {
+        //Для класса C переопределите метод Display, чтобы в консоли было "C".
+        public override void Display()
+        {
+            Console.WriteLine("C");
+        }
+    }
+    class D : B
+    {
+        //Для D снова скройте метод.
+        public new void Display()
+        {
+            Console.WriteLine("D");
+        }
+
+    }
+    class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("E");
+        }
+
+    }
+}
