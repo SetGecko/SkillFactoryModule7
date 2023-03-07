84ccc8bd0852d1d7928a7d056f47919a6e555a65
Ипполитов Алексей <setgecko@gmail.com>
2023-03-07 21:51:09 +0300

    Выполнил задание 7.2.5

---
+++ Task_7.2.5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_7._2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrivedClass obj = new DrivedClass();
+            obj.Display();
+        }
+    }
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+    class DrivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Метод класса DrivedClass");
+        }
+    }
+}
