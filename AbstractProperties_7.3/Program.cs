663285e8da72931e3f40aaab42df92613d2a4a33
Ипполитов Алексей <setgecko@gmail.com>
2023-03-09 19:04:45 +0300

    Практика по абстрактным свойствам

---
+++ AbstractProperties_7.3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AbstractProperties_7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Name = "Umka";
+            Console.WriteLine(dog.Name);
+        }
+    }
+    abstract class FourLeggedAnimal
+    {
+        public abstract string Name
+        {
+            get;
+            set;
+        }
+    }
+    class Dog : FourLeggedAnimal
+    {
+        private string name;
+            public override string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class Cat : FourLeggedAnimal
+    {
+        public override string Name
+        {
+            get;
+            set;
+        }
+    }
+}
