c6b42403bc64fe980e33d5a1c0beded9093e04e1
Ипполитов Алексей <setgecko@gmail.com>
2023-03-09 18:51:53 +0300

    Практика по абстрактным методам + Выполнил задание 7.3.3

---
+++ AbstractMethods_7.3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AbstractMethods_7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Абстрактный элемент класса — это такой элемент абстрактного класса,
+            //который не имеет реализации в базовом классе и должен быть реализован в классе-наследнике.
+            Cat cat = new Cat();
+            cat.Describe();
+        }
+    }
+    abstract class AbstractClass
+    {
+        public string Name;
+        public abstract void Display();
+    }
+
+    abstract class FourLeggedAdnimal
+    {
+        public abstract void Describe();
+    }
+    class Dog : FourLeggedAdnimal
+    {
+        public override void Describe() 
+        {
+            Console.WriteLine("Это животное  - собакен");
+        }
+    }
+    class Cat : FourLeggedAdnimal
+    {
+        public override void Describe()
+        {
+            Console.WriteLine("Это животное - котя");
+        }
+    }
+}
---
+++ Task_7.3.3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_7._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Создайте классы для следующих объектов компьютера: процессор (Processor), 
+             * материнская карта (MotherBoard), видеокарта (GraphicCard).
+             * Унаследуйте их от класса ComputerPart.
+             * Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.
+             */
+            GraphicCard graphicCard = new GraphicCard();
+            graphicCard.Describe();
+            
+            Processor processor = new Processor();
+            processor.Describe();
+
+            MotherBoard board = new MotherBoard();
+            board.Describe();
+        }
+    }
+    abstract class ComputerPart
+    {
+        public abstract void Describe();
+    }
+    class Processor : ComputerPart
+    {
+        public override void Describe()
+        {
+            Console.WriteLine("Это проц");
+        }
+    }
+    class GraphicCard : ComputerPart
+    {
+        public override void Describe()
+        {
+            Console.WriteLine("Это видяшка");
+        }
+    }
+    class MotherBoard : ComputerPart
+    {
+        public override void Describe()
+        {
+            Console.WriteLine("Это материнка");
+        }
+    }
+}
