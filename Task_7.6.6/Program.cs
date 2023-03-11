c4aa0a81f86a3d3b63c6cdb2a75f7610cc75d785
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 05:55:13 +0300

    Выполнил задание 7.6.6

---
+++ Task_7.6.6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_7._6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Record<T1, T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
+}
