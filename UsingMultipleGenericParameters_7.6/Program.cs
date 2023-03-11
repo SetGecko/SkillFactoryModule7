3e3183c7f95450b217ef4a232e2fd9a97c5433a4
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 05:51:27 +0300

    Практика 7.6 Использование нескольких универсальных параметров

---
+++ UsingMultipleGenericParameters_7.6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UsingMultipleGenericParameters_7._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<object, int, string> generic = new Generic<object, int, string>();
+        }
+    }
+    class Generic<T1, T2, T3>
+    {
+        public T1 Field;
+        public T2 Property { get; set; }
+        public void Method (T3 param)
+        {
+            
+        }
+    }
+}
