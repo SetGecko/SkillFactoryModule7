b57073f45f810f210d8bea42b2e81ddcac6154d5
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 04:29:10 +0300

    Выполнил задание 7.5.2

---
+++ Task_7.5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_7._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Obj
+    {
+        public string Name;
+        public string Description;
+        public static int MaxValue = 2000;
+    }
+}
