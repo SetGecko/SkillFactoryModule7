21d53f05993df0cba10f822dcda410db071ab02d
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 07:11:20 +0300

    Практика ограничение обощение - нипанятнаааа

---
+++ LimitationsOfGenerics_7.6/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LimitationsOfGenerics_7._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Order
+    {
+        public int Number;
+
+        public string Description;
+
+    }
+    abstract class Delivery
+    {
+        public string Address;
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+    }
+    class HomeDelivery : Delivery 
+    { 
+
+    }
+    class PickPointDelivery : Delivery
+    {
+
+    }
+    class ShopDelivery : Delivery
+    {
+
+    }
+    class PremiumService<T> where T : Order<HomeDelivery>
+    {
+        public T Order;
+    }
+}
