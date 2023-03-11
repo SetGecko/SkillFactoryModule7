568838f07cda977d06011f04ea99d24637fa92c9
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 07:37:38 +0300

    Практика Наследование обобщений

---
+++ GenericInheritance_7.6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericInheritance_7._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass<int> intVar = new DerivedClass<int>();
+            intVar.Field = 4;
+            Console.WriteLine(intVar.Field);
+
+            DerivedClass<long> longVar = new DerivedClass<long>();
+            longVar.Field = (long)7;
+            Console.WriteLine(longVar.Field);
+
+            DerivedClass<string> stringVar = new DerivedClass<string>();
+            stringVar.Field = "zz";
+            Console.WriteLine(stringVar.Field);
+        }
+    }
+    class BaseClass<T>
+    {
+        public T Field;
+    }
+    class DerivedClass<T> : BaseClass<T> 
+    { 
+        public T Property { get; set; }
+    }
+}
