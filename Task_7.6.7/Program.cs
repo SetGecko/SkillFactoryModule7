52face761bc5773ee8843fcaaa160c0be15ef359
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 06:24:20 +0300

    Выполнил задание 7.6.7

---
+++ Task_7.6.7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_7._6._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Car<T1>
+    {
+        public T1 Engine;
+        public virtual void ChangePart<T2>(T2 newPart)
+        {
+
+        }
+    }
+    class ElectricEngine
+    {
+
+    }
+    class GasEngine
+    {
+
+    }
+    class Battery
+    {
+
+    }
+    class Differential
+    {
+
+    }
+    class Wheel
+    {
+
+    }
+}
