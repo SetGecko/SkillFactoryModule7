4168ebdc3d7e83e7677e8e4a573674e8a13f8dd9
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 05:39:00 +0300

    Выполнил задание 7.6.2

---
+++ Task_7.6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_7._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Car<T>
+    {
+        public T Engine;
+    }
+    class ElectricEngine
+    {
+
+    }
+    class GasEngine
+    {
+
+    }
+}
