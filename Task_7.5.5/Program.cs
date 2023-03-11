973fe21d3d5f4ecb26262a159ecb161c434bced7
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 04:55:59 +0300

    Выполнил задание 7.5.5

---
+++ Task_7.5.5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_7._5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Obj
+    {
+        public string Name;
+        public string Description;
+
+        public static string Parent;
+        public static int DaysInWeek;
+        public static int MaxValue;
+        static Obj()
+        {
+            Parent = "System.Object";
+            DaysInWeek = 7;
+            MaxValue = 2000;
+        }
+    }
+}
