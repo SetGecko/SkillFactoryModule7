c59a14952c4cb20c92bc001496bb60589b257560
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 04:27:05 +0300

    Практика по статическим элементам класса 7.5

---
+++ Practice_7.5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_7._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleClass.MinValue);
+        }
+    }
+    class SimpleClass
+    {
+        public static int MinValue = 100;
+        public int Value;
+        public SimpleClass(int value)
+        {
+            if (value < MinValue) 
+            {
+                Value = MinValue;
+            }
+            else
+            {
+                Value = value;
+            }
+        }
+    }
+}
