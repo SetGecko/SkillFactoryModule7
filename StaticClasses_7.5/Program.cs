52a8ceb6c034814f22afae406dd3708b7b1ee57e
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 04:59:05 +0300

    Практика статические классы 7.5

---
+++ StaticClasses_7.5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StaticClasses_7._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    static class Car
+    {
+        public static int MinPrice = 100_000;
+        public static int MaxPrice;
+
+        static Car()
+        {
+            Console.WriteLine("Вызван статический конструктор класса Car");
+            MaxPrice = 1_000_000;
+        }
+    }
+}
