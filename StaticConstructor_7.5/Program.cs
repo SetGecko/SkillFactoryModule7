705f010140a0ceffc7ba7ac56f87dddd267ce0d3
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 04:52:12 +0300

    Практика по статическим конструкторам 7.5

---
+++ StaticConstructor_7.5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StaticConstructor_7._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Console.WriteLine(Car.MinPrice);
+
+            Console.WriteLine(Car.MaxPrice);
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        public static int MinPrice = 100_000;
+        public static int MaxPrice;
+        static Car()
+        {
+            Console.WriteLine("Вызван статический конструктор класса Car");
+            MaxPrice = 1_000_000;
+        }
+        public int Price;
+    }
+}
