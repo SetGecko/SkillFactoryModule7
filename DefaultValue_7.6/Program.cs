be5cf122d0a00e8394044d692dbc10e004e15fa9
Ипполитов Алексей <setgecko@gmail.com>
2023-03-11 05:47:33 +0300

    Практика по значениями по умолчанию

---
+++ DefaultValue_7.6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DefaultValue_7._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = default(decimal);
+            Console.WriteLine(a);
+
+            var b = default(object);
+            Console.WriteLine(b);
+        }
+    }
+    class Generic<T>
+    {
+        public T Field = default(T); // Поле типа T
+    }
+}
